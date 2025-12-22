bc2868577a38348ab6ed7e1acbb3a5d2698c220a
cpc <pengchengluoyi@126.com>
2025-12-22 17:57:15 +0800

    add 修复打包异常问题

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ app.include_router(websocket_router.router)
 
 @app.get("/")
 def health_check():
-    return {"status": "ok", "version": "1.0.1", "upload_dir": UPLOAD_DIR}
+    return {"status": "ok", "version": "1.0.2", "upload_dir": UPLOAD_DIR}
 
 @app.get("/get_api")
 def get_api():
