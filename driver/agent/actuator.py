ca7509a2660ef79e75a85bbc2072fa21fa48d8a9
cpc <pengchengluoyi@126.com>
2026-01-13 10:19:41 +0800

    根据 requirements.txt动态加载三方包, fixwin平台打包异常问题

--- driver/agent/actuator.py
+++ driver/agent/actuator.py
@@ -4,7 +4,6 @@ from script.log import SLog, current_run_id, current_flow_id
 from server.core.log_database import LogSessionLocal
 from server.models.log import WorkflowLog
 from driver.agent.Core.orchestrator import Orchestrator
-# from driver.brain.central_nervous_system import CentralNervousSystem
 from server.services import run_service
 from script.mTask import report
 
